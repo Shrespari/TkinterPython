67f363b90eb08b3fb1704f012d66165b04ecf6a2
Shrespari <86852064+Shrespari@users.noreply.github.com>
2024-03-29 14:10:12 -0400

    Update grid.py

--- grid.py
+++ grid.py
@@ -2,7 +2,7 @@ from tkinter import *
 
 root = Tk()
 mylabel1 = Label(root, text="Hello World!!").grid(row=0, column=0)
-mylabel2 = Label(root, text="My name is Pariksha Shrestha").grid(row=1,column=5)
+mylabel2 = Label(root, text="My name is Pari Shrestha").grid(row=1,column=5)
 #mylabel3 = Label(root, text=" ")
 
 #mylabel1.grid(row=0, column=0)
